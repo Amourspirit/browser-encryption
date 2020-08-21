1118194e9e871501c934e48c57860432175311e0
Paul Moss <bigbytetech@gmail.com>
2020-08-20 21:43:24 -0400

    inital markdonw working

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
-
 /*
  *  Main JavaScript 
  */
 // let RANDOM_INLINE_KEY = '';
 
-$(document).ready(function(){
-    window.RANDOM_INLINE_KEY = keygenJS(256, { ekey: true });
+$(document).ready(function () {
+    window.RANDOM_INLINE_KEY = keygenJS(256, {
+        ekey: true
+    });
     window.observer = lozad(); // lazy loads elements with default selector as '.lozad'
     window.observer.observe();
     // $("#reset").click(function(){
@@ -19,13 +20,13 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
     $("#chipher").on("change", function () {
         uiRefresh();
-    }); 
+    });
     $(".btn_refresh").on("click", function () {
         uiRefresh();
         // clear any generate url value when buttons have been clicked
         setValUrl('');
-    }); 
-    $('.collapse-links').on("click", function() {
+    });
+    $('.collapse-links').on("click", function () {
         let el = $('.collapse-links>span.glyph-toggle');
         if (el.length > 0) {
             if ($(this).hasClass("collapsed")) {
@@ -65,6 +66,46 @@ $(document).ready(function(){
         event.preventDefault();
         $(this).ekkoLightbox();
     });
+    $('#myModal').on('shown.bs.modal', function () {
+        $("#markdown").val(function () {
+            return $("#plain").val();
+        });
+        // # Marked in browser\n\nRendered by **marked**.
+        $(".marked-content").html(marked($("#markdown").val()));
+        $('#markdown').trigger('focus')
+    })
+    $('#edit_plain_md').on('click', function (event) {
+        event.preventDefault();
+        $("#markdown").val(function () {
+            return $("#plain").val();
+        });
+        // # Marked in browser\n\nRendered by **marked**.
+        $(".marked-content").html(marked($("#markdown").val()));
+        $('#myModal').modal('show');
+        $('#markdown').trigger('focus');
+    })
+    /**
+     * Bind the makrdown textarea and updated the makrked-content to reflect the makred up html version.
+     */
+    $('#markdown').bind('input propertychange', function () {
+        // set the content of the marked-content element to the makred up value
+        $(".marked-content").html(marked($("#markdown").val()));
+    });
+    $("#modal_save_changes").on("click", function (event) {
+        $("#plain").val($("#markdown").val());
+        $("#markdown").val('');
+        $('#myModal').modal('hide');
+    });
+    /**
+     * Monitor .makred-content and when there is a change mark all a tags to open in a new window.
+     */
+    $('.marked-content').bind('DOMSubtreeModified', function (event) {
+        event.preventDefault();
+        $('.marked-content a').each(function () {
+            $(this).attr('target', '_blank');
+        });
+    });
+
     uiRefresh();
 });
 
@@ -72,7 +113,7 @@ $(document).ready(function(){
     let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     let pathname = window.location.pathname;
     let fav = pathname;
-    
+
     fav += 'favicon.ico';
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
